09fc85cf2092e282e7f40a3d68a657547d0b42ca
menright <mde32@nau.edu>
2015-11-23 10:03:06 -0700

    temp activity changes

--- app/src/main/java/teamblowfish/ihome/TempActivity.java
+++ app/src/main/java/teamblowfish/ihome/TempActivity.java
@@ -5,12 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class TempActivity extends AppCompatActivity {
 
     private HouseAccount houseAccount;
     private String house;
     private String user;
+    private SeekBar seekBar;
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +35,40 @@ public class TempActivity extends AppCompatActivity {
         //Toast.makeText(getApplicationContext(),house,Toast.LENGTH_SHORT).show();
         houseAccount = housesDB.findHouseAccount(house);
         user = userAndHouse[0];
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        textView = (TextView) findViewById(R.id.temp);
+
+        // Initialize the textview
+//        textView.setText("Covered: " + seekBar.getProgress() + "/" + seekBar.getMax());
+        textView.setText(seekBar.getProgress() + (char) 0x00B0);
+
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progress = 68;
+
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+            }
+
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textView.setText("Covered: " + progress + "/" + seekBar.getMax());
+                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
